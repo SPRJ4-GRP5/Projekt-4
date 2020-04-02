abde5c4550583ee47c0fb48739639da6b16bf039
Randi Mikkelsen Bech <au615785@uni.au.dk>
2020-04-02 10:46:16 +0200

    Dropdown

--- Test-implementeringer/SagTestMVC/Controllers/HomeController.cs
+++ Test-implementeringer/SagTestMVC/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SagTest.Controllers
             return View();
         }
 
+        public IActionResult OwnCases()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
