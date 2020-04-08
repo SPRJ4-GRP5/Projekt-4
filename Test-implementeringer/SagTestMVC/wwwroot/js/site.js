f52bbeb0edb8c8c60449a6e8e982de8c572ec8eb
Randi Mikkelsen Bech <au615785@uni.au.dk>
2020-04-08 12:40:21 +0200

    R: overskrift

--- Test-implementeringer/SagTestMVC/wwwroot/js/site.js
+++ Test-implementeringer/SagTestMVC/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const detailView = document.querySelector('.detail');
+const detailTitle = document.querySelector('.detail-title');
+const masterItems = document.querySelectorAll('.master-item');
+
+function select(selected) {
+    for (var item of masterItems) {
+        item.classList.remove('active-item');
+    }
+    selected.classList.add('active-item');
+    detailTitle.innerHTML = selected.innerHTML;
+
+}
+
+
+//masterItems.forEach((item) => {
+//    item.addEventListener('click', function () {
+//        console.log('Clicked item');
+//        for (let item of masterItems) {
+//            item.classList.remove('active-item');
+//        }
+
+//        this.classList.add('active-item');
+//        detailTitle.innerHTML = this.innerHTML;
+//    });
+//});
